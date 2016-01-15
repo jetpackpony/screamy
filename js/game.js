ab200fa5afc27b1ab6203b18584caf5430ec3fc4
jetpackpony <jetpackpony@gmail.com>
2016-01-15 10:45:15 +0300

    Time-based frames; Added event listeners

--- js/game.js
+++ js/game.js
@@ -1,16 +1,46 @@
 var framework;
+var speed = 100; // in pixels per second
+
+var canvas;
+var ctx;
+var width;
+var height;
+var x = 10;
+var y = 10;
+var direction = 1;
 
 var drawFrame = function () {
 	console.log("FPS: " + framework.getFPS());
 	document.querySelector("#fps span").innerHTML = framework.getFPS();
+	if (framework.getInputState().up) {
+		text = "up";
+	} else {
+		text = "nothing";
+	}
+	document.querySelector("#inputStates span").innerHTML = text;
 
-	var ctx = document.querySelector("#screamy").getContext('2d');
+	ctx.clearRect(0, 0, width, height);
 	ctx.fillStyle = "red";
-	ctx.fillRect(Math.round(Math.random() * 400), Math.round(Math.random() * 400), 10, 10);
+	ctx.fillRect(x, y, 10, 10);
+
+	if (y < 10) {
+		direction = 1;
+	}
+	if (y > 350) {
+		direction = -1;
+	}
+
+	y += framework.convertSpeed(speed) * direction;
 }
 
 window.onload = function init() {
+	canvas = document.querySelector("#screamy");
+	ctx = canvas.getContext('2d');
+	width = canvas.width;
+	height = canvas.height;
+
 	framework = new GameFramework();
 	framework.setDrawFrame(drawFrame);
+	framework.setInputListeners(['up']);
 	framework.start();
 }
--- js/gameFramework.js
+++ js/gameFramework.js
@@ -1,23 +1,32 @@
 var GameFramework = function () {
 	var drawFrame;
-	var prevFrameTime = 0;
+	var lastSecondFrameTime = 0;
 	var fps = 0;
 	var fpsCounter = 0;
+	var frameDelta = 0;
+	var prevFrameTime = 0;
+	var inputStates = {};
 	
 	var getFPS = function () {
 		return fps;
-	}
+	};
 
 	var countFPS = function (time) {
-		if (time - prevFrameTime > 1000) {
-			prevFrameTime = time;
+		if (time - lastSecondFrameTime > 1000) {
+			lastSecondFrameTime = time;
 			fps = fpsCounter;
 			fpsCounter = 0;
 		}
 		fpsCounter++;
 	};
 
+	var	countFrameDelta = function (time) {
+		frameDelta = time - prevFrameTime;
+		prevFrameTime = time;
+	};
+
 	var mainLoop = function (time) {
+		countFrameDelta(time);
 		countFPS(time);
 
 		drawFrame();
@@ -29,11 +38,45 @@ var GameFramework = function () {
 		requestAnimationFrame(mainLoop);
 	};
 
+	var convertSpeed = function (speed) {
+		return speed * (frameDelta / 1000);
+	};
+
+	/**
+	 * Input listeners
+	 */
+	var eventListener = function (listenedKey, event, state) {
+		if (listenedKey === 'up' && event.keyCode === 38) {
+			inputStates[listenedKey] = state;
+		}
+	};
+
+	var setInputListeners = function (keys) {
+		keys.forEach(function (el, i, arr) {
+			window.addEventListener('keydown', function (event) {
+				eventListener(el, event, true);
+			}, false);
+			window.addEventListener('keyup', function (event) {
+				eventListener(el, event, false);
+			}, false);
+		});
+	};
+
+	var getInputState = function () {
+		return inputStates;
+	};
+
+	/** 
+	 * Framework's public interface
+	 */
 	return {
 		start: start,
 		setDrawFrame: function (drawFrameFunction) {
 			drawFrame = drawFrameFunction;
 		},
-		getFPS: getFPS
-	}
+		getFPS: getFPS,
+		getInputState: getInputState,
+		setInputListeners: setInputListeners,
+		convertSpeed: convertSpeed
+	};
 };

81204f70440b033891580f01ae46d2b531ee2a3f
jetpackpony <jetpackpony@gmail.com>
2016-03-15 11:25:57 +0700

    Added the center point to polygon; Removed tests for private methods; Added broad collision detection to collision module

--- js/Collisions.js
+++ js/Collisions.js
@@ -11,6 +11,10 @@ Collisions = (function() {
     return new Vector(this.x - other.x, this.y - other.y);
   };
 
+  Vector.prototype.add = function(other) {
+    return new Vector(this.x + other.x, this.y + other.y);
+  };
+
   Vector.prototype.perpendicular = function() {
     return new Vector(this.y, -this.x);
   };
@@ -21,6 +25,10 @@ Collisions = (function() {
 
   Vector.prototype.isEqual = function(other) {
     return this.x === other.x && this.y === other.y;
+  };
+
+  Vector.prototype.lengthSquared = function() {
+    return this.dotProduct(this);
   };
 
 
@@ -38,38 +46,43 @@ Collisions = (function() {
   Projection.prototype.isEqual = function(that) {
     return this.axis.isEqual(that.axis) && this.x1 === that.x1 && this.x2 === that.x2;
   };
-  COL.Projection = Projection;
 
 
 
 
-
-
-  function Polygon(points) {
-    this.points = points;
+  function Polygon(points, center) {
+    this._relativePoints = points;
+    this._calcPoints = [];
+    this.setCenter(center || new Vector());
   }
   COL.Polygon = Polygon;
 
-  Polygon.prototype.isCollidingWith = function (other) {
-    // Calc all axes
-    var axes = [].concat(this.getAxes()).concat(other.getAxes());
-
-    for (var i = 0; i < axes.length; i++) {
-      p1 = this.projectOn(axes[i]);
-      p2 = other.projectOn(axes[i]);
-      if (!p1.overlap(p2)) {
-        return false;
-      }
+  Polygon.prototype.setCenter = function(new_center) {
+    this._center = new_center;
+    for (var i = 0; i < this._relativePoints.length; i++) {
+      this._calcPoints.push(this._center.add(this._relativePoints[i]));
     }
-    return true;
   };
 
-  Polygon.prototype.projectOn = function(axis) {
-    var cur, min, max;
-    min = max = axis.dotProduct(this.points[0]);
+  Polygon.prototype.getCenter = function() {
+    return this._center;
+  };
 
-    for (var i = 0; i < this.points.length; i++) {
-      cur = axis.dotProduct(this.points[i]);
+  Polygon.prototype.getCalcPoints = function() {
+    return this._calcPoints;
+  };
+
+  Polygon.prototype.getRelativePoints = function() {
+    return this._relativePoints;
+  };
+
+
+  var makeProjection = function(polygon, axis) {
+    var cur, min, max;
+    min = max = axis.dotProduct(polygon.getCalcPoints()[0]);
+
+    for (var i = 0; i < polygon.getCalcPoints().length; i++) {
+      cur = axis.dotProduct(polygon.getCalcPoints()[i]);
       if (cur <  min)
         min = cur;
       if (cur > max)
@@ -78,15 +91,65 @@ Collisions = (function() {
     return new Projection(min, max, axis);
   };
 
-  Polygon.prototype.getAxes = function() {
+  var getAxesForPolygon = function (polygon) {
     var p1, p2;
     var axes = [];
-    for (var i = 0; i < this.points.length; i++) {
-      p1 = this.points[i];
-      p2 = this.points[i + 1 == this.points.length ? 0 : i + 1];
+    for (var i = 0; i < polygon.getCalcPoints().length; i++) {
+      p1 = polygon.getCalcPoints()[i];
+      p2 = polygon.getCalcPoints()[i + 1 == polygon.getCalcPoints().length ? 0 : i + 1];
       axes.push(p1.substract(p2).perpendicular());
     }
     return axes;
+  };
+
+  var isPreciseCollision = function (polygon1, polygon2) {
+    // Calc all axes
+    var axes = [].concat(getAxesForPolygon(polygon1)).concat(getAxesForPolygon(polygon2));
+
+    for (var i = 0; i < axes.length; i++) {
+      p1 = makeProjection(polygon1, axes[i]);
+      p2 = makeProjection(polygon2, axes[i]);
+      if (!p1.overlap(p2)) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  var getPolygonRadius = function (polygon) {
+    var relPoints = polygon.getRelativePoints();
+    var max = 0;
+
+    for (var i = 0; i < relPoints.length; i++) {
+      if (max < relPoints[i].lengthSquared()) {
+        max = relPoints[i].lengthSquared();
+      }
+    }
+    return Math.sqrt(max);
+  };
+
+  Polygon.prototype.getRadius = function() {
+    if (!this._radius) {
+      return this._radius = getPolygonRadius(this);
+    }
+    return this._radius;
+  };
+
+  var isBroadCollision = function (polygon1, polygon2) {
+    var center_distance, rad_distance;
+    center_distance = polygon1.getCenter().substract(polygon2.getCenter());
+    center_distance = center_distance.dotProduct(center_distance);
+    rad_distance = (polygon1.getRadius() + polygon2.getRadius());
+    return center_distance < rad_distance * rad_distance;
+  };
+
+  Polygon.prototype.isCollidingWith = function (other) {
+    if (isBroadCollision(this, other)) {
+      if (isPreciseCollision(this, other)) {
+        return true;
+      }
+    }
+    return false;
   };
 
   Polygon.CreateNewFromPoints = function() {
--- spec/CollisionsSpec.js
+++ spec/CollisionsSpec.js
@@ -1,7 +1,6 @@
 describe("Collisions:", function() {
   require("../js/Collisions.js");
   var Vector = Collisions.Vector;
-  var Projection = Collisions.Projection;
   var Polygon = Collisions.Polygon;
 
 
@@ -16,6 +15,12 @@ describe("Collisions:", function() {
       var v3 = v1.substract(v2);
       expect(v3.x).toEqual(1);
       expect(v3.y).toEqual(-3);
+    });
+
+    it("should add vectors correctly", function() {
+      var v3 = v1.add(v2);
+      expect(v3.x).toEqual(5);
+      expect(v3.y).toEqual(5);
     });
 
     it("should find perpendicular vector correctly", function() {
@@ -35,26 +40,10 @@ describe("Collisions:", function() {
       expect(v3.isEqual(v1)).toBeTruthy();
       expect(v3.isEqual(v2)).toBeFalsy();
     });
-  });
 
-  describe("Projection:", function() {
-    var p1, p2;
-    var axis = new Vector(1, 1);
-
-    beforeEach(function() {
-      p1 = new Projection(0, 2, axis);
-      p2 = new Projection(1, 3, axis);
-      p3 = new Projection(3, 4, axis);
-    });
-
-    it("should detect overlap between overlapping projections", function() {
-      expect(p1.overlap(p2)).toBeTruthy();
-    });
-    it("should detect overlap between touching projections", function() {
-      expect(p2.overlap(p3)).toBeTruthy();
-    });
-    it("should NOT detect overlap between NOT overlapping projections", function() {
-      expect(p1.overlap(p3)).toBeFalsy();
+    it("should calculate length squared correctly", function() {
+      expect(v1.lengthSquared()).toEqual(10);
+      expect(v2.lengthSquared()).toEqual(20);
     });
   });
 
@@ -63,64 +52,32 @@ describe("Collisions:", function() {
 
     beforeEach(function() {
       p1 = new Polygon([
+        new Vector(-2, 0),
         new Vector(0, 2),
-        new Vector(2, 3),
-        new Vector(3, 2),
-        new Vector(3, 1),
-        new Vector(1, 0)
-      ]);
+        new Vector(1, 1),
+        new Vector(1, -1),
+        new Vector(-1, -2)
+      ], new Vector(2, 2));
 
       p2 = new Polygon([
-        new Vector(3, 0),
-        new Vector(3, 3),
-        new Vector(6, 2)
-      ]);
+        new Vector(-1, 2),
+        new Vector(2, 0),
+        new Vector(-1, -2)
+      ], new Vector(4, 2));
 
       p3 = new Polygon([
-        new Vector(4, 2),
-        new Vector(5, 3),
-        new Vector(7, 1),
-        new Vector(6, 0)
-      ]);
-    });
-
-    describe("Inner calculations:", function() {
-      var axes1 = [
-        new Vector(-1, 2),
-        new Vector(1, 1),
-        new Vector(1, 0),
-        new Vector(1, -2),
-        new Vector(-2, -1)
-      ];
-      
-      var axes2 = [
-        new Vector(-3, 0), 
-        new Vector(1, 3),
-        new Vector(2, -3)
-      ];
-
-      var axes3 = [
         new Vector(-1, 1),
-        new Vector(2, 2),
-        new Vector(1, -1),
-        new Vector(-2, -2)
-      ];
+        new Vector(2, 0),
+        new Vector(2, -1),
+        new Vector(1, -2)
+      ], new Vector(5, 2));
 
-      var proj1_2_3 = new Projection(-6, 3, axes2[2]);
-      var proj2_1_2 = new Projection(3, 8, axes1[1]);
-      var proj3_3_4 = new Projection(-16, -12, axes3[3]);
-
-      it("should calculate axes for it's sides correctly", function() {
-        expect(p1.getAxes()).toContainAllVectors(axes1);
-        expect(p2.getAxes()).toContainAllVectors(axes2);
-        expect(p3.getAxes()).toContainAllVectors(axes3);
-      });
-
-      it("should calculate it's projection on an axis correctly", function() {
-        expect(p1.projectOn(axes2[2])).toEqual(proj1_2_3);
-        expect(p2.projectOn(axes1[1])).toEqual(proj2_1_2);
-        expect(p3.projectOn(axes3[3])).toEqual(proj3_3_4);
-      });
+      p4 = new Polygon([
+        new Vector(-1, 1),
+        new Vector(2, 0),
+        new Vector(2, -1),
+        new Vector(1, -2)
+      ], new Vector(10, 2));      
     });
 
     describe("Collsion detection:", function() {
@@ -135,6 +92,10 @@ describe("Collisions:", function() {
       it("should not detect collision with not overlapping polygon", function() {
         expect(p1.isCollidingWith(p3)).toBeFalsy();
       });
+
+      it("should not detect collision with not overlapping polygon", function() {
+        expect(p1.isCollidingWith(p4)).toBeFalsy();
+      });      
     });
 
     describe("Polygon creation:", function() {

1d6a529457712b1c535bf0bc9891a913b2467482
jetpackpony <jetpackpony@gmail.com>
2016-03-29 11:41:43 +0700

    Created a proxy for Canvas Context object

--- js/GameFramework/CanvasOperations.js
+++ js/GameFramework/CanvasOperations.js
@@ -1,6 +1,57 @@
 GameFramework = (function ($) {
   var canvas, ctx, width, height;
 
+  /**
+   * This is a proxy for the canvas context to record all calls to canvas and aid testing
+   * @param {Context} the actual 2d context of the canvas
+   */
+  var CanvasProxy = function (context) {
+    var _ctx = context;
+    var _calls = [];
+          
+    var methods = {
+      fill: function() {
+        _ctx.fill();
+      },
+      lineTo: function(x, y) {
+        _ctx.lineTo(x, y);
+      },
+      moveTo: function(x, y) {
+        _ctx.moveTo(x, y);
+      },
+      stroke: function() {
+        _ctx.stroke();
+      },
+      test: function() {
+        // just for testing purposes
+      }
+    };
+
+    var scope = this;
+    var addMethod = function(name, method) {
+      scope[methodName] = function() {
+        record(name, arguments);
+        method.apply(scope, arguments);
+      };
+    }
+          
+    for(var methodName in methods) {
+      addMethod(methodName, methods[methodName]);
+    }
+
+    var record = function(methodName, args) {
+      _calls.push({name: methodName, args: args});
+    };
+
+    this.getCalls = function() {
+      return _calls;
+    }
+
+    this.getActualContext = function () {
+      return _ctx;
+    }
+  };
+
   $._drawFrame = function (time) {
     // Draw the thing
     ctx.clearRect(0, 0, width, height);
@@ -20,7 +71,7 @@ GameFramework = (function ($) {
 
   $.setCanvas = function (new_canvas) {
     canvas = new_canvas;
-    ctx = canvas.getContext('2d');
+    ctx = new CanvasProxy(canvas.getContext('2d'));
     width = canvas.width;
     height = canvas.height;
     return $;

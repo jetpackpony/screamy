3e50fc46f876fafd8b08b951f1d772be036b6d25
jetpackpony <jetpackpony@gmail.com>
2016-04-14 09:44:19 +0700

    Added player implementation

--- js/player.js
+++ js/player.js
@@ -1,34 +1,35 @@
-function Player(options, context, framework) {
+var GF = GameFramework;
 
-  options.x = 50;
-  options.y = 30;
+function Player() {
+  GF.DrawableObject.apply(this, arguments);
 
-  DrawableObject.call(this, options, context, framework);
+  var frames = [],
+      image = new Image(),
+      w, h, sprite;
+  image.src = "img/png/flying/spritesheet_50_flying.png";
+  h = 60;
+  w = 61;
+  for(var i = 0; i < 4; i++) {
+    frames.push({ x: w * i, y: 0, h: h, w: w });
+  }
 
-  this.setSprite({
-    // ctx: context,
-    imageURL: "img/png/flying/spritesheet_50_flying.png",
-    frameWidth: 61,
-    frameHeight: 60,
-    framesNum: 4
-  });
+  sprite = 
+    (new GF.Sprite())
+    .setImage(image)
+    .addFrames(frames)
+    .setFrameDelay(100);
 
-
+  this.setSprite(sprite);
+  this.setPosition(new GF.Vector(50, 30));
 }
-Player.prototype = new DrawableObject({}, null, null);
-Player.prototype.draw = function () {
-  var x = this._coordinates.x - 52 / 2;
-  var y = this._coordinates.y - 50 / 2;
 
-  this.drawSprite(x, y);
+Player.prototype = new GF.DrawableObject();
+Player.prototype.constructor = Player;
 
-  // Changing the position
-  if (this._coordinates.y > maxY) {
-    this._coordinates.y = maxY;
-    this.stop();
-  }
-  if (this._coordinates.y < minY) {
-    this._coordinates.y = minY;
-    this.stop();
-  }
+Player.prototype.updateState = function() {
+  GF.DrawableObject.prototype.updateState.apply(this, arguments);
+
+  // if (this.isCollidingWith(GF.ground)) {
+    
+  // }
 };
